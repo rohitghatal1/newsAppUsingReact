0407e32ba4bcda606b5cb3b3c7b544d3efe8df4b
rohitghatal1 <rohitghatal@gmail.com>
2024-07-21 18:03:05 +0545

    added bootstrap card and new api

--- src/components/News.js
+++ src/components/News.js
@@ -5,8 +5,32 @@ export class News extends Component {
   render() {
     return (
       <div>
-        This is a news component.
-        <NewsItem/>
+        <div className="container mt-3">
+          <div className="row">
+              <div className="col-md-4">
+                  <NewsItem title ="Smart watch" desc = "The best smart watch"/>
+              </div>
+
+              <div className="col-md-4">
+                <NewsItem title = "Smart watch 2" desc="Another version of smart watch"/>
+              </div>
+
+              <div className="col-md-4">
+                <NewsItem/>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-4">
+                <NewsItem/>
+              </div>
+              <div className="col-md-4">
+                <NewsItem/>
+              </div>
+              <div className="col-md-4">
+                <NewsItem/>
+              </div>
+            </div>
+          </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-
 export class NewsItem extends Component {
   render() {
+    let {title, desc} = this.props;
     return (
       <div>
-        This is a news item.
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://www.cnet.com/a/img/resize/770007077c38361ca13ef0b0aa4c208f88fa2053/hub/2023/09/20/9e9b8e2b-04ee-4087-b819-c99ec8bbc980/apple-watch-ultra-2-7.jpg?auto=webp&fit=crop&height=1200&width=1200" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
+            <a href="/" className="btn btn-dark text-light">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
