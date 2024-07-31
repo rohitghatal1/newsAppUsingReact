abb324fd338bb4a6d375b1cd353f26ccc36f0a56
rohitghatal1 <rohitghatal@gmail.com>
2024-07-31 16:53:28 +0545

    added code to dynamically display news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,9 @@ export class NewsItem extends Component {
         <div className="card" style={{width: "18rem"}}>
           <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{desc}</p>
-            <a href="/newsdetail/" className="btn btn-sm btn-dark text-light">Read more</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{desc}...</p>
+            <a href={newsUrl} rel='noreferrer' target='_blank' className="btn btn-sm btn-dark text-light">Read more</a>
           </div>
         </div>
       </div>
