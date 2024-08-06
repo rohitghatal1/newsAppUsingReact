9306b0d3ceeb211aef77ff25508eb1f4b3b90fa2
rohitghatal1 <rohitghatal@gmail.com>
2024-08-06 14:53:01 +0545

    downloaded package for top loading bar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
+  state={
+    progress : 0
+  }
+
+  setProgress(progress){
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <div>
-        <Navbar/>
-        <News/>
+        <Navbar />
+        <LoadingBar
+          color='#f11946'
+          progress={this.state.progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+        <News />
       </div>
     )
   }
